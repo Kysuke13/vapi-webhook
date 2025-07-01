407c6622dbd19fc3bf8c5c3eeb99a66fbab45adc
Kysuke13 <150520781+Kysuke13@users.noreply.github.com>
2025-07-01 17:56:44 +0200

    Update trigger-audio.js

--- api/trigger-audio.js
+++ api/trigger-audio.js
@@ -1,6 +1,6 @@
 // pages/api/get-audio.js
 export default async function handler(req, res) {
-  const audioUrl = https://vpxeoycjmrsxbcmocwly.supabase.co/storage/v1/object/public/son//nom.mp3"; // Ton fichier audio public
+  const audioUrl = "https://vpxeoycjmrsxbcmocwly.supabase.co/storage/v1/object/public/son//nom.mp3"; // Ton fichier audio public
 
   res.status(200).json({
     type: "play",
